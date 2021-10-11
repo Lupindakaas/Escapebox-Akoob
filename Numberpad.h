216bcc3560726acc6c71fb5d794dc92455b2e4d5
lupindakaas <lucasbdekker@hotmail.com>
2021-10-11 22:16:08 +0200

    Initial commit

---
+++ Numberpad.h
@@ -0,0 +1,17 @@
+class Numberpad
+{
+private:
+    /* data */
+public:
+    Numberpad(/* args */);
+    
+    ~Numberpad();
+};
+
+Numberpad::Numberpad(/* args */)
+{
+}
+
+Numberpad::~Numberpad()
+{
+}

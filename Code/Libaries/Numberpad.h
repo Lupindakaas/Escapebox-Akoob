0154bffa320222a80cf50d28dec0a980c0cc05c2
lupindakaas <lucasbdekker@hotmail.com>
2021-10-11 22:51:16 +0200

    Some folder reorganisation

---
+++ Code/Libaries/Numberpad.h
@@ -0,0 +1,18 @@
+class Numberpad
+{
+private:
+    int[5] rowPins;
+    int[5] columnPins;
+public:
+    Numberpad(int[5] rows, int[5] columns);
+    
+    ~Numberpad();
+};
+
+Numberpad::Numberpad(int[5] rows, int[5] columns)
+{
+}
+
+Numberpad::~Numberpad()
+{
+}

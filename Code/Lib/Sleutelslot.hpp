a323a28ccb00508f74addb5617680c4bb04a369d
lupindakaas <lucasbdekker@hotmail.com>
2021-11-15 23:19:28 +0100

    Sleutelslot.hpp opgemaakt

--- Code/Lib/Sleutelslot.hpp
+++ Code/Lib/Sleutelslot.hpp
@@ -1,33 +1,36 @@
-class Sleutelslot {
+class Sleutelslot{
 private:
-    uint8_t waarde; //0 = vrij, 1 = op slot
-	uint8_t PIN; // De pin waar het slot op aangesloten zit.
+	uint8_t waarde; //0 = vrij, 1 = op slot
+	uint8_t PIN;	// De pin waar het slot op aangesloten zit.
 public:
-    Sleutelslot(uint8_t pin);
-    ~Sleutelslot();
+	Sleutelslot(uint8_t pin);
+	~Sleutelslot();
 	void leesAf();
 	uint8_t OpSlot();
 };
 
 //---------------Source code----------------//
 
-Sleutelslot::Sleutelslot(uint8_t pin) {
+Sleutelslot::Sleutelslot(uint8_t pin)
+{
 	PIN = pin;
 	pinMode(PIN, INPUT);
 	vanSlotAf();
 }
 
-Sleutelslot::~Sleutelslot() {
+Sleutelslot::~Sleutelslot()
+{
 	vanSlotAf();
 }
 
-void Sleutelslot::leesAf() {
+void Sleutelslot::leesAf()
+{
 
 	waarde = digitalRead(PIN);
-
 }
 
-bool Sleutelslot::status() {
-    leesAf();
-    return waarde;
+bool Sleutelslot::status()
+{
+	leesAf();
+	return waarde;
 }

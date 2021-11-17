1579971e2a4f29960689425dea12184562913371
lupindakaas <lucasbdekker@hotmail.com>
2021-11-17 18:19:19 +0100

    InitialCommit

---
+++ Code/Main/Sleutelsysteem.cpp
@@ -0,0 +1,36 @@
+/*
+//Author: Lucas Dekker en Akoob 
+
+//Bevat:
+
+	2 elektronische sleutelsloten
+	2 2 bit seven segment displays
+	1 matrix geschakeld getalinvoer paneel
+	1 ladeslot
+	1 resetknop
+	1 arduino nano
+
+//Doel/program flow:
+
+
+
+*/
+
+#include "Code\lib\Sleutelslot.hpp"
+#include "Code\lib\SevenSegmentDisplay.hpp"
+#include "Code\lib\Numpad.hpp"
+#include "Code\Lib\Drukschakelaar.hpp"
+#include "Code\lib\Ladeslot.hpp"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
